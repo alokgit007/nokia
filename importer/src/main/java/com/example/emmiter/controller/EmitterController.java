4037bfaf482501889b5fa12ef461b3d6437aa22d
alokgit007 <69386370+alokgit007@users.noreply.github.com>
2021-09-22 17:48:59 +0530

    Update EmitterController.java

--- importer/src/main/java/com/example/emmiter/controller/EmitterController.java
+++ importer/src/main/java/com/example/emmiter/controller/EmitterController.java
@@ -1,23 +1,19 @@
 package com.example.emmiter.controller;
 
 
-import org.apache.tomcat.util.http.HeaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.emmiter.repository.Employee;
 import com.example.emmiter.service.AlertService;
 import com.example.emmiter.service.EmitterService;
-
-
-
 
 @RestController
 public class EmitterController {
@@ -25,12 +21,10 @@ public class EmitterController {
 	EmitterService emitterService;
 	
     AlertService alertService;
-    
-    private String applicationName;
 
 	
 	private final Logger log = LoggerFactory.getLogger(AlertService.class);
-	private static final String ENTITY_NAME = "employee";
+
 
 	
 	@Autowired
@@ -41,22 +35,18 @@ public class EmitterController {
 	}
 	
 	@PostMapping("/import")
-	public  ResponseEntity<Employee> saveData(@RequestBody Employee employee){
-		Employee result = emitterService.saveData(employee);
-        return ResponseEntity.created(new URI("/api/stores/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
-            .body(result);
+	public  ResponseEntity saveData(@RequestBody Employee employee){
+		 emitterService.saveData(employee);
+		 return new ResponseEntity<>("employee data saved successfully",HttpStatus.CREATED);
 	}
 
 	@PutMapping("/modify")
-	public  ResponseEntity<Employee>  updateData(@RequestBody Employee employee){
+	public  ResponseEntity  updateData(@RequestBody Employee employee){
 		Employee result=emitterService.updateData(employee);
 		log.debug("SEND store alert for Store: {}", employee);
 		alertService.alertStoreStatus(result);
 		
-		return ResponseEntity.ok()
-				            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, employee.getId()))
-				            .body(result);
+		return new ResponseEntity<>("employee data updated successfully",HttpStatus.OK);
 	}
 
 }
